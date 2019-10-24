f37bc87d6b0d440aa7b8fc2040e7cc7982f48c07
Christian DeTamble <christian.detamble@outlook.com>
2019-10-25 00:31:38 +0200

    fixed base url

--- CreateHtmlFiles/Program.cs
+++ CreateHtmlFiles/Program.cs
@@ -12,7 +12,7 @@ namespace CreateHtmlFiles
     {
         static void Main(string[] args)
         {
-            var builder = new Builder("/mouthlessgames/", true);
+            var builder = new Builder("/", true);
             
             builder.CreateGeneric("about", "About");
 			builder.CreateGeneric("legal", "Legal");
